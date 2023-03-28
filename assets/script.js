6167d41038eb14f3c364787302236dd6ab072d8f
tbohn2 <tanner.bohn@yahoo.com>
2023-03-27 21:09:27 -0700

    Added comments in JS

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,24 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elementshour9
-// in the html.
+// Variable set
 var button = $(".saveBtn")
 var input = $(".description")
 var today = dayjs()
 var time = today.format('H')
-// var time = 10
+
+// Array of time slots identified by the ID
 var hours = [$("#hour-9"), $("#hour-10"), $("#hour-11"), $("#hour-12"), $("#hour-13"), $("#hour-14"), $("#hour-15"), $("#hour-16"), $("#hour-17")]
 
+// The date is displayed using today variable formatted
 $('#currentDay').text(today.format('dddd, MMMM D, YYYY'))
 
+// Changes the background color of the time slots
 for (let i = 0; i < hours.length; i++) {
+  // Sets the time equal to the i + 9 for comparing to the time slot IDs
   const hour = i + 9;
+  // Sets a var for the time slot IDs
   const hourID = hours[i].attr("id")
+  // Retrieves the to-do for the hour from local storage using the hour ID as the key under which it was stored
   hours[i].children(".description").val(localStorage.getItem(hourID))
+  // If the time is later, equal or earlier than the time slot shown, the background will change by adding a class
   if (hour < time) {
     hours[i].children(".description").addClass("past")
   }
@@ -25,38 +30,14 @@ for (let i = 0; i < hours.length; i++) {
   }
 }
 
+// Saves to-do in local storage under key equal to time slot ID
 button.on("click", function (event) {
   var btnClicked = $(event.target)
+  // Finds the input, which is a sibling of the button clicked, and stores its text as a variable
   var todo = btnClicked.siblings(".description").val()
+  // Finds the value of the id of the parent of the button, which is the time slot container
   var key = btnClicked.parents().attr("id")
-  console.log(key)
-  console.log(todo);
-  // get id of parent and make that key
+  // Stores user input in local storage under the key equal to the ID of the time slot where the input was placed
   localStorage.setItem(key, todo)
-  // localStorage.setItem("key", JSON.stringify(var))
 })
 
-
-
-// localStorage.getItem("key")
-// $(function () {
-// 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-// });
